ba4e0953b59dad0e1ad98bdafb7ec502f48f72c1
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-22 01:21:51 +0530

    modified cell ele. with index

--- Grid.js
+++ Grid.js
@@ -1,11 +1,31 @@
 import { CELL_SIZE, GRID_SIZE, CELL_GAP } from "./constants.js";
 
 export default class Grid {
+  #cells;
+
   constructor(gridElement) {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
-    createCellElement(gridElement);
+
+    this.#cells = createCellElement(gridElement).map((cellEle, idx) => {
+      const [x, y] = [idx % GRID_SIZE, Math.floor(idx / GRID_SIZE)];
+      const cell = new Cell(cellEle, x, y);
+
+      return cell;
+    });
+  }
+}
+
+class Cell {
+  #cellElement;
+  #x;
+  #y;
+
+  constructor(cellElement, x, y) {
+    this.#cellElement = cellElement;
+    this.#x = x;
+    this.#y = y;
   }
 }
 
