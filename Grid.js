81f49283ce55fefff567ba84da6ef9f77abdac1c
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-22 00:42:55 +0530

    fixed typo

--- Grid.js
+++ Grid.js
@@ -1,9 +1,9 @@
 import { CELL_SIZE, GRID_SIZE, CELL_GAP } from "./constants.js";
 
 export default class Grid {
-  constructor(gridElemenr) {
-    gridElemenr.style.setProperty("--grid-size", GRID_SIZE);
-    gridElemenr.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
-    gridElemenr.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+  constructor(gridElement) {
+    gridElement.style.setProperty("--grid-size", GRID_SIZE);
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
   }
 }
