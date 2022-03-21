1c7b81de82fd6528b13b1a0d402d8ef49120242e
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-22 00:56:57 +0530

    added dynamic cell creation

--- Grid.js
+++ Grid.js
@@ -5,5 +5,25 @@ export default class Grid {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+    createCellElement(gridElement);
   }
 }
+
+const createSingleCell = () => {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+
+  return cell;
+};
+
+const createCellElement = (gridElement) => {
+  const cells = [];
+
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = createSingleCell();
+    cells.push(cell);
+    gridElement.append(cell);
+  }
+
+  return cells;
+};
