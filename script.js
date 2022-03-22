15617091b0db1d9fc35b0173cb19692bc2c75efb
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-23 01:33:07 +0530

    UI action setup

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ const gameBoard = document.getElementById("game-board");
 const grid = new Grid(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
+
+setupInput();
+
+const moveUp = () => {};
+const moveDown = () => {};
+const moveLeft = () => {};
+const moveRight = () => {};
+
+function setupInput() {
+  window.addEventListener("keydown", handleInput, {
+    once: true,
+  });
+}
+
+function handleInput(event) {
+  switch (event.key) {
+    case "ArrowUp": {
+      moveUp();
+      break;
+    }
+
+    case "ArrowDown": {
+      moveDown();
+      break;
+    }
+
+    case "ArrowLeft": {
+      moveLeft();
+      break;
+    }
+
+    case "ArrowRight": {
+      moveRight();
+      break;
+    }
+
+    default: {
+      setupInput();
+      return;
+    }
+  }
+  setupInput();
+}
