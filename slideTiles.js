be08641bef00457db455de8a31cbbd02b59f7843
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-29 02:28:30 +0530

    bundled promises while transitioning

--- slideTiles.js
+++ slideTiles.js
@@ -1,25 +1,32 @@
 export default (cells) => {
-  cells.forEach((group) => {
-    for (let i = 1; i < group.length; i++) {
-      const cell = group[i];
+  Promise.all(
+    cells.flatMap((group) => {
+      const promises = [];
 
-      if (cell.tile === null) continue;
+      for (let i = 1; i < group.length; i++) {
+        const cell = group[i];
 
-      let lastValidCell = null;
+        if (cell.tile === null) continue;
 
-      for (let j = i - 1; j >= 0; j--) {
-        const moveToCell = group[j];
+        let lastValidCell = null;
 
-        if (moveToCell.canAccept(cell.tile)) lastValidCell = moveToCell;
-        else break;
+        for (let j = i - 1; j >= 0; j--) {
+          const moveToCell = group[j];
+
+          if (moveToCell.canAccept(cell.tile)) lastValidCell = moveToCell;
+          else break;
+        }
+
+        if (lastValidCell !== null) {
+          promises.push(cell.tile.waitForTransition());
+
+          if (lastValidCell.tile !== null) lastValidCell.mergeTile = cell.tile;
+          else lastValidCell.tile = cell.tile;
+        }
+
+        cell.tile = null;
       }
-
-      if (lastValidCell !== null) {
-        if (lastValidCell.tile !== null) lastValidCell.mergeTile = cell.tile;
-        else lastValidCell.tile = cell.tile;
-      }
-
-      cell.tile = null;
-    }
-  });
+      return promises;
+    })
+  );
 };
