5d6187995e7720e929efea365d89a10b39e882ff
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-29 00:46:16 +0530

    added mergeTile prop.

--- Cell.js
+++ Cell.js
@@ -3,15 +3,36 @@ export default class Cell {
   #x;
   #y;
   #tile;
+  #mergeTile;
 
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement;
     this.#x = x;
     this.#y = y;
+    this.#tile = null;
+  }
+
+  get x() {
+    return this.#x;
+  }
+
+  get y() {
+    return this.#y;
   }
 
   get tile() {
     return this.#tile;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (!value) return;
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
   }
 
   set tile(value) {
@@ -19,5 +40,12 @@ export default class Cell {
     if (!value) return;
     this.#tile.x = this.#x;
     this.#tile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile === null ||
+      (this.mergeTile == null && this.#tile.value === tile.value)
+    );
   }
 }
